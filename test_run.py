890d1dbe4f05847c17520d0d48eabbf3e5aef8ec
Ayeh Jehoshaphat Ojoniko <jehoshaphatayeh@gmail.com>
2026-01-03 13:11:13 +0100

    Add test run script

---
+++ test_run.py
@@ -0,0 +1,31 @@
+from src.matching import rank_schools_for_student
+
+student = {
+    "name": "John Doe",
+    "gpa": 3.4,
+    "budget": 15000,
+    "field": "Computer Science",
+    "preferred_countries": ["Canada", "UK"]
+}
+
+schools = [
+    {
+        "name": "University of Toronto",
+        "country": "Canada",
+        "min_gpa": 3.2,
+        "tuition": 14000,
+        "fields": ["Computer Science", "Engineering"]
+    },
+    {
+        "name": "University of Manchester",
+        "country": "UK",
+        "min_gpa": 3.5,
+        "tuition": 16000,
+        "fields": ["Computer Science", "Data Science"]
+    }
+]
+
+results = rank_schools_for_student(student, schools)
+
+for school, score in results:
+    print(f"{school}: {score}")
